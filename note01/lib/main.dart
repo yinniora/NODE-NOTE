23c34bc2d9dc0adf92dfc62577c8a9ee95065e0e
ice-orange <zhangjiaxin23@foxmail.com>
2021-01-20 17:31:08 +0800

    ''

--- note01/lib/main.dart
+++ note01/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'widget/dayNumSelector.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,12 +33,16 @@ class RandomWorlds extends StatefulWidget {
 
 class _RandomWorldsState extends State<RandomWorlds> {
   var nodeController = new TextEditingController();
+  var _focusNode = new FocusNode();
+  var dayNumCon = new TextEditingController();
+  var dayNumFocus = new FocusNode();
+  int hintT = 1;
   var _storageString = '';
   var noteData = {};
 
   //存储所有的提醒事项的数组
   var noteList = List();
-  bool selected = false;
+  bool selected = true;
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +58,12 @@ class _RandomWorldsState extends State<RandomWorlds> {
             child: Row(children: [
               new Container(
                 child: IconButton(
-                   icon: new Icon(Icons.bookmark_border),
-                  onPressed: ()=>{
-                     setState((){
-                       selected = false;
-                     })
+                  icon: new Icon(Icons.bookmark_border),
+                  onPressed: () => {
+                    setState(() {
+                      selected = false;
+                      FocusScope.of(context).requestFocus(_focusNode);
+                    })
                   },
                 ),
                 margin: EdgeInsets.fromLTRB(0, 15, 0, 10),
@@ -67,13 +73,14 @@ class _RandomWorldsState extends State<RandomWorlds> {
                   onTap: () {
                     setState(() {
                       selected = false;
+                      FocusScope.of(context).requestFocus(_focusNode);
                     });
                   },
                   child: Center(
                     child: AnimatedContainer(
-                      height: selected ? 60 : 330,
-                      duration: Duration(seconds: 2), //动画持续时间
-                      curve: Curves.fastLinearToSlowEaseIn, //差值器（动画效果）
+                      height: selected ? 60 : 350,
+                      duration: Duration(seconds: 1), //动画持续时间
+                      curve: Curves.fastOutSlowIn, //差值器（动画效果）
                       child: Card(
                         child: ListView(
                           shrinkWrap: true,
@@ -108,21 +115,24 @@ class _RandomWorldsState extends State<RandomWorlds> {
                                     new Container(
                                       child: TextField(
                                         controller: nodeController,
+                                        focusNode: _focusNode,
                                         keyboardType: TextInputType.text,
                                         maxLines: selected ? 2 : 7,
                                         decoration: InputDecoration(
                                           contentPadding: EdgeInsets.fromLTRB(
                                               15, 30, 10, 0),
                                           labelText: '',
-                                          hintText: "",
+                                          hintText: '',
                                           helperText: '',
                                           border: OutlineInputBorder(
                                               borderRadius:
                                                   BorderRadius.circular(5),
                                               borderSide: BorderSide.none),
+                                          fillColor: Color(0xffFFEFD5),
                                           filled: true,
                                         ),
-                                        autofocus: true,
+                                        textInputAction: TextInputAction.done,
+                                        autofocus: false,
                                         style: TextStyle(
                                           fontSize: 20,
                                         ),
@@ -130,7 +140,7 @@ class _RandomWorldsState extends State<RandomWorlds> {
                                     )
                                   ],
                                 )),
-                                //提交按钮
+                                //时间日期选项
                                 new Container(
                                   margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
                                   child: Row(
@@ -139,42 +149,98 @@ class _RandomWorldsState extends State<RandomWorlds> {
                                     children: [
                                       new IconButton(
                                         icon: new Icon(Icons.schedule),
-                                        onPressed: () =>_showTimePicker(),
-                                      ), //设置时间
+                                        onPressed: () => _showTimePicker(),
+                                      ),
+                                      //设置时间
                                       new IconButton(
-                                          icon: new Icon(
-                                              Icons.today_outlined),
-                                          onPressed: () => _showDataPicker()), //日期
-                                      new IconButton(
-                                        icon: new Icon(Icons.calendar_view_day_outlined),
-                                        onPressed: () => _dayNumSelector(),
-                                      )//天数
+                                          icon: new Icon(Icons.today_outlined),
+                                          onPressed: () => _showDataPicker()),
+                                      //日期
+                                      dayNumSelector(dayNumCon: dayNumCon, dayNumFocus: dayNumFocus), //天数
                                     ],
                                   ),
                                 ),
+                                //提交按钮
                                 new Container(
-                                    alignment: Alignment.center,
-                                    margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
-                                    child: Container(
-                                      height: 30,
-                                      width: 100,
-                                      decoration: new BoxDecoration(
-                                          color: Colors.lightBlueAccent,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10.0))),
-                                      child: MaterialButton(
-                                          onPressed: (){
-                                            setState(() {
-                                              selected = true;
-                                            });
-                                          },
-                                          child: Text(
-                                            "保存",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontWeight: FontWeight.w600),
-                                          )),
-                                    ))
+                                    margin: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            new Container(
+                                                child: Container(
+                                              height: 30,
+                                              width: 100,
+                                              decoration: new BoxDecoration(
+                                                  color: Colors.lightBlueAccent,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(
+                                                              10.0))),
+                                              child: MaterialButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      selected = true;
+                                                      _focusNode.unfocus();
+                                                    });
+                                                  },
+                                                  child: Text(
+                                                    "保存",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontWeight:
+                                                            FontWeight.w600),
+                                                  )),
+                                            )),
+                                            new Container(
+                                                child: Container(
+                                              height: 30,
+                                              width: 100,
+                                              decoration: new BoxDecoration(
+                                                  color: Colors.redAccent,
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(
+                                                              10.0))),
+                                              child: MaterialButton(
+                                                  onPressed: () {
+                                                    nodeController.clear();
+                                                    dayNumCon.clear();
+                                                    FocusScope.of(context)
+                                                        .requestFocus(
+                                                            _focusNode);
+                                                  },
+                                                  child: Text(
+                                                    "重置",
+                                                    style: TextStyle(
+                                                        color: Colors.white,
+                                                        fontWeight:
+                                                            FontWeight.w600),
+                                                  )),
+                                            ))
+                                          ],
+                                        ),
+                                        new Container(
+                                          alignment: Alignment.center,
+                                          height: 30,
+                                          margin: EdgeInsets.only(top: 5),
+                                          child: IconButton(
+                                            icon: Icon(
+                                              Icons.keyboard_arrow_up,
+                                              color: Colors.grey,
+                                            ),
+                                            onPressed: () {
+                                              setState(() {
+                                                selected = true;
+                                              });
+                                              _focusNode.unfocus();
+                                            },
+                                          ),
+                                        )
+                                      ],
+                                    )),
                               ],
                             ),
                           ],
@@ -226,15 +292,16 @@ class _RandomWorldsState extends State<RandomWorlds> {
   //时间选择器和日期选择器
   var _time;
   var _date;
-  var _dayNum;
+
   _showTimePicker() async {
     var picker =
-    await showTimePicker(context: context, initialTime: TimeOfDay.now());
+        await showTimePicker(context: context, initialTime: TimeOfDay.now());
     setState(() {
       _time = picker.toString();
     });
     print(_time);
   }
+
   _showDataPicker() async {
     Locale myLocale = Localizations.localeOf(context);
     var picker = await showDatePicker(
@@ -244,34 +311,10 @@ class _RandomWorldsState extends State<RandomWorlds> {
         lastDate: DateTime(2040),
         locale: myLocale);
     setState(() {
-      _date =picker.toString().substring(0,9);
+      _date = picker.toString().substring(0, 9);
     });
     print(_date);
   }
-  _dayNumSelector() {
-    var dayList = [];
-    for(int i=1;i<=30;i++){
-      dayList.add('${i}');
-    }
-    print(dayList);
-    var datListStr = dayList.join(',');
-    print(datListStr);
-    return DropdownButton(
-      onChanged: (String newValue){
-        setState(() {
-          _dayNum = newValue;
-        });
-        print(_dayNum);
-      },
-      items: <String>[datListStr].map<DropdownMenuItem<String>>((String value) {
-      return DropdownMenuItem<String>(
-        value: value,
-        child: Text(value),
-      );
-    }).toList(),
-    );
-  }//天数选择
-
 
 
   //利用SharedPreferences存储数据
@@ -317,3 +360,5 @@ class _RandomWorldsState extends State<RandomWorlds> {
         nodeController.value.text.toString());
   }
 }
+
+
---
+++ note01/lib/widget/dayNumSelector.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class dayNumSelector extends StatelessWidget {
+  const dayNumSelector({
+    Key key,
+    @required this.dayNumCon,
+    @required this.dayNumFocus,
+  }) : super(key: key);
+
+  final TextEditingController dayNumCon;
+  final FocusNode dayNumFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        height: 20,
+        width: 90,
+        child: Row(
+          children: [
+            new Container(
+              margin:
+              EdgeInsets.only(right: 5),
+              child: Text('持续'),
+            ),
+            new Expanded(child:
+            new TextField(
+              controller: dayNumCon,
+              focusNode: dayNumFocus,
+              maxLines: 1,
+              keyboardType:
+              TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter
+                    .digitsOnly,
+                LengthLimitingTextInputFormatter(
+                    3)
+              ],
+              textInputAction:
+              TextInputAction.done,
+              autofocus: false,
+              decoration: InputDecoration(
+                contentPadding:
+                EdgeInsets.fromLTRB(
+                    5, 0, 0, 0),
+                hintText: '1',
+                border: OutlineInputBorder(
+                    borderRadius:
+                    BorderRadius
+                        .circular(8),
+                    borderSide: BorderSide(
+                      color:
+                      Colors.blueGrey,
+                      style:
+                      BorderStyle.solid,
+                    )),
+              ),
+            )
+            ),
+            new Container(
+              margin:
+              EdgeInsets.only(left: 5),
+              child: Text('天'),
+            )
+          ],
+        ));
+  }
+}
